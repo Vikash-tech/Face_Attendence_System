cc41e4e8f5988816a0476e926d01d43b0723e8fa
Vikash_Tech <127883196+Vikash-tech@users.noreply.github.com>
2026-03-19 20:29:52 +0530

    Update service.py

--- service.py
+++ service.py
@@ -26,7 +26,7 @@ def load_api_base_url():
     except Exception:
         pass
 
-    return "https://chelsea-partner-sleeping-advancement.trycloudflare.com"
+    return "https://agree-aspect-pregnant-measured.trycloudflare.com"
 
 
 API_BASE_URL = load_api_base_url()
